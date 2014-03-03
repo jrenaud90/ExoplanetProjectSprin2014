59fcceef010a082b8b15471484ef4587a7b614fb
Joe Renaud <jrenaud@masonlive.gmu.edu>
2014-03-03 15:46:10 -0500

    HW5 V1

---
+++ Scripts/ReadData.m
@@ -0,0 +1,8 @@
+function [ output_args ] = ReadData(filename,varargin)
+%READDATA Summary of this function goes here
+%   Detailed explanation goes here
+%http://www.youtube.com/watch?v=2jgQ9xaT3oQ
+A = fscanf(filename
+
+end
+
