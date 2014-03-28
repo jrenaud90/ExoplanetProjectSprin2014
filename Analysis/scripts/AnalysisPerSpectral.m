6696ded2b7b604f986b8d0787f6b514411a988e0
Joe Renaud <jrenaud@masonlive.gmu.edu>
2014-03-28 13:15:08 -0400

    3-28 V2

--- Analysis/scripts/AnalysisPerSpectral.m
+++ Analysis/scripts/AnalysisPerSpectral.m
@@ -28,23 +28,28 @@ end
 
 sr2=['M','K','G'];
 for k=1:3
-    figure(k)
+    figure('Name',num2str(k),'Visible','off')
     for j=1:3
         subplot(1,3,j)
         if k == 1
             hist(Radius(j,:),ceil(sum(~isnan(Radius(j,:)))/5));
             sr = strcat('Radius:',sr2(j),',Sample:',Rst{j});
             xlabel(sr)
+            file2='Radii';
         elseif k == 2
-            hist(Mass(j,:),ceil(sum(~isnan(Mass(j,:)))/5));
+            hist(Mass(j,:),ceil(sum(~isnan(Mass(j,:)))/20));
             sr = strcat('Mass:',sr2(j),',Sample:',Mst{j});
             xlabel(sr)
+            file2='Mass';
         else
-            hist(Period(j,:),ceil(sum(~isnan(Period(j,:)))/5));
+            hist(Period(j,:),ceil(sum(~isnan(Period(j,:)))/20));
             sr = strcat('Period:',sr2(j),',Sample:',Pst{j});
             xlabel(sr)
+            file2='Period';
         end
     end
+    file=['APS_out\',file2,'.png'];
+    print('-dpng',file)
 end
 
 %%Resample%%%
@@ -80,22 +85,27 @@ end
 
 sr2=['M','K','G'];
 for k=4:6
-    figure(k)
+    figure('Name',num2str(k),'Visible','off')
     for j=1:3
         subplot(1,3,j)
-        if k == 1
-            hist(RadiusBoot(j,:),ceil(sum(~isnan(RadiusBoot(j,:)))/5));
+        if k == 4
+            hist(RadiusBoot(j,:),ceil(sum(~isnan(RadiusBoot(j,:)))/(10*Comp)));
             sr = strcat('Radius:',sr2(j),',Sample:',Rst{j});
             xlabel(sr)
-        elseif k == 2
-            hist(MassBoot(j,:),ceil(sum(~isnan(RadiusBoot(j,:)))/5));
+            file2='BootStrappedRadii';
+        elseif k == 5
+            hist(MassBoot(j,:),ceil(sum(~isnan(MassBoot(j,:)))/(40*Comp)));
             sr = strcat('Mass:',sr2(j),',Sample:',Mst{j});
             xlabel(sr)
+            file2='BootStrappedMass';
         else
-            hist(PeriodBoot(j,:),ceil(sum(~isnan(RadiusBoot(j,:)))/5));
+            hist(PeriodBoot(j,:),ceil(sum(~isnan(PeriodBoot(j,:)))/(40*Comp)));
             sr = strcat('Period:',sr2(j),',Sample:',Pst{j});
             xlabel(sr)
+            file2='BootStrappedPeriod';
         end
     end
+    file=['APS_out\',file2,'.png'];
+    print('-dpng',file)
 end
         
